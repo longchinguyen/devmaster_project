9a72685806f1fc3aa0451214e8c66cacb36950d5
shanglong1998@gmail.com <shanglong1998@gmail.com>
2019-03-13 18:04:51 +0700

    create_db

---
+++ app/Http/Controllers/homcontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homcontroller extends Controller
+{
+    public function index()
+    {
+    	echo"chao controller";
+    }
+}
